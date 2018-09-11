157fe2455d03198e36fa1ecd2cb7e2bb50400f11
manisha <manishak@gdiz.com>
2018-09-11 17:48:17 +0530

    update

--- app/src/main/java/com/example/manisha/dashboard/MainActivity.java
+++ app/src/main/java/com/example/manisha/dashboard/MainActivity.java
@@ -1,16 +1,62 @@
 package com.example.manisha.dashboard;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 {
+    LinearLayout first;
+    LinearLayout second;
+    LinearLayout third;
+    LinearLayout fourth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first=findViewById(R.id.first);
+        second=findViewById(R.id.second);
+        third=findViewById(R.id.third);
+        fourth=findViewById(R.id.fourth);
+
+        first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,First.class);
+                startActivity(intent1);
+            }
+        });
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,Second.class);
+                startActivity(intent1);
+            }
+        });
+        third.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,Third.class);
+                startActivity(intent1);
+            }
+        });
+        fourth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,Fourth.class);
+                startActivity(intent1);
+            }
+        });
 
     }
 
